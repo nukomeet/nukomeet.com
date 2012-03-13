fd215f4da23155a0a6754424d4141cf7599aa8d3
zaiste <oh@zaiste.net>
2012-03-14 00:37:19 +0100

    deploy conf, post command added

---
+++ commands/post.rb
@@ -0,0 +1,39 @@
+usage       'create:post [options]'
+aliases     :cp
+summary     'create a new blog post'
+description ''
+
+flag   :h, :help,  'show help for this command' do |value, cmd|
+  puts cmd.help
+  exit 0
+end
+
+run do |opts, args, cmd|
+  title = args.first
+  puts "Creating a blog post with a name '#{title}'. Stand by..."
+
+  date = Time.now
+  #strftime("%Y_%m") 
+  
+  filename, path = calc_path(title, date)
+
+  template = <<TEMPLATE
+---
+created_at: #{date}
+kind: article
+publish: true
+title: "#{title}"
+---
+
+TODO: Add content to `#{path}.`
+TEMPLATE
+
+  File.open(path, 'w') { |f| f.write(template) }
+  puts "\t[ok] Edit #{path}"
+end
+
+def calc_path(title, date)
+  filename = "#{date.strftime('%Y-%m')}_#{title.split.join('_')}.md"
+  path = "content/blog/#{filename}"
+  [filename, path]
+end
