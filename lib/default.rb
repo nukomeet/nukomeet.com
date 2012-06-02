cc99872f5de46d6e84cfac1cb49af53d4701e6a0
Zaiste! <oh@zaiste.net>
2012-06-02 18:35:32 +0200

    current nav chosen

--- lib/default.rb
+++ lib/default.rb
@@ -4,3 +4,11 @@
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
+
+def link_to_with_current(text, path, desc)
+  if @item_rep and @item_rep.path == path
+    "<li class='current'>#{link_to(text, path, :'data-description' => desc)}</li>"
+  else
+    "<li>#{link_to(text, path, :'data-description' => desc)}</li>"
+  end
+end 
