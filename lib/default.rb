351bb06552115797773318fc6e1f529f16e29b35
Bartłomiej Wiaduch <tukan2can@gmail.com>
2014-12-11 11:54:02 +0100

    improv: simplified - removed gzip and cache buster

--- lib/default.rb
+++ lib/default.rb
@@ -1,13 +1,11 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
-require 'nanoc/cachebuster'
 
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::XMLSitemap
 include Nanoc3::Helpers::Tagging
-include Nanoc::Helpers::CacheBusting
 
 def link_to_with_current(text, path, desc)
   if @item_rep and @item_rep.path == path
