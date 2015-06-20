896690238bbaac9e68ab3c9f5d2829482018708b
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-20 21:03:50 +0200

    Upgrade to nanoc 4 (basic)

--- lib/default.rb
+++ lib/default.rb
@@ -1,11 +1,11 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
-include Nanoc3::Helpers::Blogging
-include Nanoc3::Helpers::LinkTo
-include Nanoc3::Helpers::Rendering
-include Nanoc3::Helpers::XMLSitemap
-include Nanoc3::Helpers::Tagging
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::XMLSitemap
+include Nanoc::Helpers::Tagging
 
 def link_to_with_current(text, path, desc)
   if @item_rep and @item_rep.path == path
